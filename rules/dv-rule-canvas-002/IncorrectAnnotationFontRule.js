2fdec54a47d86b35edab1704c2aebe600ceabe69
tsigle <terrysigle@pingidentity.com>
2024-05-01 21:11:57 +0000

    CLOUD-212 - Enanche font family by cleaning

--- rules/dv-rule-canvas-002/IncorrectAnnotationFontRule.js
+++ rules/dv-rule-canvas-002/IncorrectAnnotationFontRule.js
@@ -1,25 +1,36 @@
 const LintRule = require("../../lib/LintRule");
-const LintCodes = require("../../config/lint-codes.json")
+const LintCodes = require("../../config/lint-codes.json");
 
 class IncorrectAnnotationColorRule extends LintRule {
   runRule() {
     try {
       const dvFlow = this.mainFlow;
 
-
       const annotationNodes = this.getNodesByType("annotationConnector");
       const fontFamily = "sans-serif";
 
       annotationNodes.forEach((node) => {
         const { data } = node;
-        if (data.properties?.fontFamily?.value?.toLowerCase() !== fontFamily.toLowerCase()) {
+        if (
+          data.properties?.fontFamily?.value?.toLowerCase() !==
+          fontFamily.toLowerCase()
+        ) {
           this.addError("dv-bp-annotation-002", {
             messageArgs: [data.properties.fontFamily.value, data.id],
             recommendationArgs: [LintCodes["dv-bp-annotation-001"].reference],
             nodeId: data.id,
           });
-        }
 
+          console.log(this.cleanFlow);
+          if (this.cleanFlow) {
+            data.properties.fontFamily.value = fontFamily;
+            this.addCleanResult(
+              "Set annotation font to sans-serif",
+              data.id,
+              "fontFamily"
+            );
+          }
+        }
       });
     } catch (err) {
       this.addError("generic-error", { messageArgs: [`${err}`] });
