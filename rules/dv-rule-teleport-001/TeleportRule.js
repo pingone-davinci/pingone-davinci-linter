8ff23e49681ee0d4e2f4816585cafcbbaeb0fd42
tsigle <terrysigle@pingidentity.com>
2024-05-01 21:09:19 +0000

    CLOUD-211 - Enhance teleport rule

--- rules/dv-rule-teleport-001/TeleportRule.js
+++ rules/dv-rule-teleport-001/TeleportRule.js
@@ -28,6 +28,44 @@ class TeleportRule extends LintRule {
           this.addError("dv-er-teleport-001", {
             messageArgs: [`${nodeTitle} (${nodeId})`],
           });
+        } else {
+          // Check if the goto node has the correct input schema
+          const gotoNodeInputSchema =
+            this.getNodeById(nodeId).data.properties.inputSchema?.value;
+
+          let gotoNodeInputSchemaJSON = {};
+          if (gotoNodeInputSchema) {
+            gotoNodeInputSchemaJSON = JSON.parse(gotoNodeInputSchema);
+          }
+
+          // Get all nodes with the instanceId of the goto node
+          const nodes =
+            this.mainFlow?.enabledGraphData?.elements?.nodes?.filter((node) =>
+              node.data?.properties?.nodeInstanceId?.value?.match(nodeId)
+            );
+
+          nodes?.forEach((node) => {
+            // get all schema items from properties, except nodeInstanceId
+            const callingSchema = Object.keys(node.data.properties).filter(
+              (prop) => prop !== "nodeInstanceId"
+            );
+
+            callingSchema.forEach((attrName) => {
+              if (gotoNodeInputSchemaJSON.properties[attrName] === undefined) {
+                this.addError("dv-er-teleport-002", {
+                  messageArgs: [attrName],
+                });
+
+                if (this.cleanFlow) {
+                  const { data } = node;
+                  delete data.properties[attrName];
+                  this.addCleanResult(
+                    `Removed teleport goto node attribute ${attrName}`
+                  );
+                }
+              }
+            });
+          });
         }
       });
     } catch (err) {
