ad4d931d29ad0f0bbecd01e838402d94fd3b6a4e
tsigle <terrysigle@pingidentity.com>
2024-05-01 20:57:52 +0000

    eslint cleanup

--- test/dv-rules.test.js
+++ test/dv-rules.test.js
@@ -1,25 +1,28 @@
-const DVLinter = require('pingone-davinci-linter')
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+const DVLinter = require("pingone-davinci-linter");
 
 const rules = DVLinter.getRules();
 
 const testRuleWithJson = function (rule, tests) {
   tests?.forEach((t) => {
     const testInput = require(`../${t}`);
-    const testExpect = require("../" + t.replace('.json', '.expect.json'));
+    const testExpect = require(`../${t.replace(".json", ".expect.json")}`);
 
     test(`Testing ${rule} with ${t}`, () => {
       const linter = new DVLinter(testInput);
-      expect(linter.lintFlow({
-        includeRules: rule
-      })).toMatchObject(testExpect);
+      expect(
+        linter.lintFlow({
+          includeRules: rule,
+        })
+      ).toMatchObject(testExpect);
     });
-  })
-}
+  });
+};
 
-for (const r in rules) {
+Object.keys(rules).forEach((r) => {
   const rule = rules[r];
   rule.tests?.forEach((f) => {
     testRuleWithJson(r, rule.tests);
-  })
-}
-
+  });
+});
